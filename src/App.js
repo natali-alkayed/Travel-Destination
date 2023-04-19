94ec5d5e40998d38ff058d3ac7f8b04e26693a1a
natali-alkayed <nataly2000alkayed@gmail.com>
2023-04-19 10:46:24 +0000

    bransh added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Home from "./components/home/Home.js"
+// import Tours from "./components/tours/Tours.js"
+// const data = require ('../src/data/db.json')
+export default function App() {
+  return (
+<>
+<Home/>
+
+</>
+  )
+}
---
+++ src/components/header/Header.js
@@ -0,0 +1,11 @@
+import './Header.css'
+function Header(){
+    return(
+        <h1> Toures App </h1>
+    )
+}
+export default Header;
+
+
+
+
